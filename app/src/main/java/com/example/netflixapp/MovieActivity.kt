19bb5a5e574eb0dfa8ebc7ecc9b5265f450f96b2
JoseLeandroNascimento <j.leandro1998silva@gmail.com>
2025-02-18 00:24:49 -0500

    feat: add toobal personalizada

---
+++ app/src/main/java/com/example/netflixapp/MovieActivity.kt
@@ -0,0 +1,26 @@
+package com.example.netflixapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MovieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_movie)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val toolBar: Toolbar = findViewById(R.id.movie_toobar)
+        setSupportActionBar(toolBar)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.baseline_arrow_back_24)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
